9d0bc51641b6426752843475a340dae96bc9524a
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2024-02-15 23:37:50 +0530

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -1,5 +1,34 @@
 #include <unistd.h> 
 //contains operating system API functions
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Structure to store original terminal attributes
+struct termios orig_termios;
+
+// Function to restore original terminal attributes
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+// Function to enable raw mode in the terminal
+void enableRawMode() {
+  // Get the current terminal attributes and store them in orig_termios
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  
+  // Register disableRawMode to be called automatically when the program exits
+  atexit(disableRawMode);
+  
+  // Create a copy of orig_termios to modify (so we don't modify the original)
+  struct termios raw = orig_termios;
+  
+  // Turn off the ECHO flag in the local flags field (c_lflag) to disable echoing
+  raw.c_lflag &= ~(ECHO);
+  
+  // Set the modified terminal attributes (raw) for the standard input file descriptor
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() { 
   char c; 
